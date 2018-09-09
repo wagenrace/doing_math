52bec2e7be5864ee4825aa199cfcfe5643e5c055
tomni <tom.nijhof@cytosmart.com>
2018-09-09 14:55:56 +0200

    [Start screen] added basic start screen with one button, no background

---
+++ Assets/scripts/StartMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+	public string game_scene_name;
+
+	public void start_button(){
+		SceneManager.LoadScene(game_scene_name);
+	}
+}
