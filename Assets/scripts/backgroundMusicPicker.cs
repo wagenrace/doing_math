a294f8969d26dcef0d11a6766ccce35cb28c55ea
tomni <tom.nijhof@cytosmart.com>
2018-09-22 17:24:15 +0200

    Make music play in all scene and seperated from sound effects

--- Assets/scripts/backgroundMusicPicker.cs
+++ Assets/scripts/backgroundMusicPicker.cs
@@ -1,17 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Audio;
 using System;
-using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-{
-	public static AudioManager instance;
-
+public class backgroundMusicPicker : MonoBehaviour {
+	public static backgroundMusicPicker instance;
 	public AudioMixerGroup mixerGroup;
-
+	public string current_song = String.Empty;
 	public Sound[] sounds;
-
-	void Awake()
-	{
+	
+	// Use this for initialization
+	void Awake () {
 		if (instance != null)
 		{
 			Destroy(gameObject);
@@ -26,18 +26,23 @@ public class AudioManager : MonoBehaviour
 		{
 			s.source = gameObject.AddComponent<AudioSource>();
 			s.source.clip = s.clip;
-			s.source.loop = s.loop;
+			s.source.loop = true;
 
 			s.source.outputAudioMixerGroup = mixerGroup;
 		}
 	}
-
-	void Start(){
-		
+	
+	// Update is called once per frame
+	void Start () {
+		if(sounds.Length > 0 && current_song == String.Empty){
+			Play(sounds[0]);
+			current_song = sounds[0].name;
+		} else if(current_song != String.Empty){
+			change_music(current_song);
+		}
 	}
 
-	public void Play(string sound)
-	{
+	public void change_music(string sound){
 		Sound s = Array.Find(sounds, item => item.name == sound);
 		if (s == null)
 		{
@@ -45,10 +50,14 @@ public class AudioManager : MonoBehaviour
 			return;
 		}
 
+		current_song = sound;
+		Play(s);
+	}
+	void Play(Sound s)
+	{
 		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
 		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
 
 		s.source.Play();
 	}
-
 }
