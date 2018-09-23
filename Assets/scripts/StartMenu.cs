b43ea84cd9c0ec3b933a832881b74b00f7730a52
tomni <tom.nijhof@cytosmart.com>
2018-09-23 22:20:37 +0200

    added credits menu

--- Assets/scripts/StartMenu.cs
+++ Assets/scripts/StartMenu.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
 	public string game_scene_name;
+	public string start_scene_name = "Start_menu";
+	public string credits_scene_name = "credits";
 
 	public void start_button(){
+		SceneManager.LoadScene(start_scene_name);
+	}
+
+	public void credits_button(){
+		SceneManager.LoadScene(credits_scene_name);
+	}
+
+	public void game_button(){
 		SceneManager.LoadScene(game_scene_name);
 	}
 }
