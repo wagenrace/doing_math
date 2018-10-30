812545cb8c7501a92a560b1c963b95b6076d7693
tomni <tom.nijhof@cytosmart.com>
2018-10-30 19:53:05 +0100

    BUGFIX: dead enemies cannot trigger deadzone

--- Assets/scripts/deadZone.cs
+++ Assets/scripts/deadZone.cs
@@ -6,9 +6,11 @@ public class deadZone : MonoBehaviour {
     public gameDirector game_director;
 
     void OnTriggerEnter2D (Collider2D other){
-		if((other.gameObject.GetComponent("enemyObject") as enemyObject) != null){
-			game_director.dead_zone_trigged();
-      other.gameObject.GetComponent<enemyObject>().dead_zone_trigged();
-		}
+      if((other.gameObject.GetComponent("enemyObject") as enemyObject) != null){
+        if(other.gameObject.GetComponent<enemyObject>().is_alive){
+          game_director.dead_zone_trigged();
+          other.gameObject.GetComponent<enemyObject>().dead_zone_trigged();
+        }
+      }
     }
 }
